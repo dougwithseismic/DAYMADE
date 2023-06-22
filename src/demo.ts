b9dcb4657c248ba0c8a03462cb48df78e0f81403
Doug Silkstone <doug@withseismic.com>
2023-06-23 01:13:34 +0200

    Update console .loggery

--- src/demo.ts
+++ src/demo.ts
@@ -1,3 +1,4 @@
+import generateRandomTicket from './utility/generate-random-ticket'
 import { processTicket } from './winning-calculator'
 
 console.log('A Winning DAYMADE Ticket')
@@ -15,3 +16,13 @@ console.log('---------')
 console.log('NEW: A new Triple Chance draw that has 3 pools to match.')
 processTicket('TRIPLE_CHANCE', [3, 7, 22, 33, 4, 2, 23, 11, 20], [3, 7, 21, 33, 4, 9, 22, 11, 20])
 console.log('---------')
+
+// loop through 30 times to call generateRandomTicket and process it.
+
+let counter = 0
+while (counter <= 150) {
+  const ticket = generateRandomTicket()
+  processTicket(ticket.prizeDraw, ticket.result, ticket.ticket)
+  counter ++
+  console.log("------------------")
+}
